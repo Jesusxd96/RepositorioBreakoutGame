2aacb9aae44a547a751a6a6057ff0304148a4551
Jesusxd96 <33816389+Jesusxd96@users.noreply.github.com>
2024-09-08 18:06:55 -0600

    Movimiento Paddle y prep work
    Se ha codificado el movimiento del paddle.
    Se le han otorgado las tags "player", "block" y "ball" a los distintos elementos que van a interactuar dentro del proyecto.

--- Breakout Game/Assets/_Scripts/Jugador.cs
+++ Breakout Game/Assets/_Scripts/Jugador.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Jugador : MonoBehaviour
 {
+    [SerializeField] private int limiteX = 23;
+    [SerializeField] private float VelocidadPaddle = 15f;
+    Vector3 mousePos2D;
+    Vector3 mousePos3D;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,34 @@ public class Jugador : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        PaddleMoving();
+    }
+
+    void PaddleMoving()
+    {
+        /*
+        mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        */
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.down * VelocidadPaddle * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.up * VelocidadPaddle * Time.deltaTime);
+        }
+
+        Vector3 pos = transform.position;
+        //pos.x = mousePos3D.x;
+        if(pos.x < -limiteX)
+        {
+            pos.x = -limiteX;
+        }else if(pos.x > limiteX)
+        {
+            pos.x = limiteX;
+        }
+        transform.position = pos;
     }
 }
